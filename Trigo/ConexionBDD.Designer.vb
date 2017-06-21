7a13e7c50d67883e1adfd4763dc40247ec6c21da
mikeCq <miguelcarrilloqnz@hotmail.com>
2017-06-21 12:34:37 -0600

    Se agregaron controles de la ventana junto con sus nombres

--- Trigo/ConexionBDD.Designer.vb
+++ Trigo/ConexionBDD.Designer.vb
@@ -25,9 +25,9 @@ Partial Class ConexionBDD
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TxUsuario = New System.Windows.Forms.TextBox()
+        Me.TxConexion = New System.Windows.Forms.TextBox()
+        Me.TxNombreBDD = New System.Windows.Forms.TextBox()
         Me.BtSalir = New System.Windows.Forms.Button()
         Me.BtGuardar = New System.Windows.Forms.Button()
         Me.BtNuevo = New System.Windows.Forms.Button()
@@ -38,53 +38,56 @@ Partial Class ConexionBDD
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(68, 89)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "ID Usuario:"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(68, 125)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.Size = New System.Drawing.Size(68, 13)
         Me.Label2.TabIndex = 0
-        Me.Label2.Text = "Label1"
+        Me.Label2.Text = "ID Conexion:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Location = New System.Drawing.Point(68, 163)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.Size = New System.Drawing.Size(132, 13)
         Me.Label3.TabIndex = 0
-        Me.Label3.Text = "Label1"
+        Me.Label3.Text = "Nombre de base de datos:"
         '
-        'TextBox1
+        'TxUsuario
         '
-        Me.TextBox1.Location = New System.Drawing.Point(216, 86)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.TxUsuario.Location = New System.Drawing.Point(216, 86)
+        Me.TxUsuario.Name = "TxUsuario"
+        Me.TxUsuario.Size = New System.Drawing.Size(142, 20)
+        Me.TxUsuario.TabIndex = 1
         '
-        'TextBox2
+        'TxConexion
         '
-        Me.TextBox2.Location = New System.Drawing.Point(216, 122)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 1
+        Me.TxConexion.Location = New System.Drawing.Point(216, 122)
+        Me.TxConexion.Name = "TxConexion"
+        Me.TxConexion.Size = New System.Drawing.Size(142, 20)
+        Me.TxConexion.TabIndex = 1
         '
-        'TextBox3
+        'TxNombreBDD
         '
-        Me.TextBox3.Location = New System.Drawing.Point(216, 160)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 1
+        Me.TxNombreBDD.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.TxNombreBDD.Location = New System.Drawing.Point(216, 160)
+        Me.TxNombreBDD.MaxLength = 15
+        Me.TxNombreBDD.Name = "TxNombreBDD"
+        Me.TxNombreBDD.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TxNombreBDD.Size = New System.Drawing.Size(142, 20)
+        Me.TxNombreBDD.TabIndex = 1
         '
         'BtSalir
         '
         Me.BtSalir.Font = New System.Drawing.Font("Arial", 8.0!)
-        Me.BtSalir.Location = New System.Drawing.Point(432, 12)
+        Me.BtSalir.Location = New System.Drawing.Point(245, 12)
         Me.BtSalir.Name = "BtSalir"
         Me.BtSalir.Size = New System.Drawing.Size(71, 40)
         Me.BtSalir.TabIndex = 9
@@ -115,13 +118,13 @@ Partial Class ConexionBDD
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(657, 397)
+        Me.ClientSize = New System.Drawing.Size(458, 249)
         Me.Controls.Add(Me.BtSalir)
         Me.Controls.Add(Me.BtGuardar)
         Me.Controls.Add(Me.BtNuevo)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TxNombreBDD)
+        Me.Controls.Add(Me.TxConexion)
+        Me.Controls.Add(Me.TxUsuario)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -135,9 +138,9 @@ Partial Class ConexionBDD
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TxUsuario As TextBox
+    Friend WithEvents TxConexion As TextBox
+    Friend WithEvents TxNombreBDD As TextBox
     Friend WithEvents BtSalir As Button
     Friend WithEvents BtGuardar As Button
     Friend WithEvents BtNuevo As Button
--- Trigo/ConexionBDD.vb
+++ Trigo/ConexionBDD.vb
@@ -1,3 +1,19 @@
-﻿Public Class ConexionBDD
+﻿Imports System.Data.SqlClient
+Imports System.Data.Sql
+Public Class ConexionBDD
+    Private Sub ConexionBDD_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub BtNuevo_Click(sender As Object, e As EventArgs) Handles BtNuevo.Click
+
+    End Sub
+
+    Private Sub BtGuardar_Click(sender As Object, e As EventArgs) Handles BtGuardar.Click
+
+    End Sub
+
+    Private Sub BtSalir_Click(sender As Object, e As EventArgs) Handles BtSalir.Click
+        Close()
+    End Sub
 End Class
